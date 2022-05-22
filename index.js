7c1f7d10a528186e8a5afdd66adccae40ec3a477
jeremiahomueti <jeremiahomueti@gmail.com>
2022-05-23 00:00:30 +0100

    update

---
+++ index.js
@@ -0,0 +1,26 @@
+/* 
+For a long time in JavaScript, if you wanted to declare a 
+variable, you had to use the 'Var' keyword - but starting
+with ES6 in in 2015, you can now declare variables with 
+Let and Const
+
+
+
+Let does not allow you declare the same variable twice.
+
+For example: 
+
+let highIQ = Young Sheldon;
+let highEQ = Young Missy;
+
+...which is good, because you don't want to be able to declare 
+the same variable twice, unless you intend to replace the value
+in the variable -- well in that case, it's best for such a process
+to have a different syntax from that of declaring a variable, 
+which Let does so beautifully. You see? Now my friend, take a 
+glass of water, sit down and let's stair into the far ends of 
+this beautiful ocean, as we as we sail on this yatch, and enjoy the relaxing breeze of the 
+dark night, the harmony of the band players, breathe!! frankSinatra
+will join us in here shortly, and we shall chat even further about
+the beauties of Let and Const.
+*/
